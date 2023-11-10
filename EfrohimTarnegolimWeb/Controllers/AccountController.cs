39e581f93bb32b1d909f9f3960440ccc96669899
Tomer Fikler <88192266+tfikler@users.noreply.github.com>
2023-11-10 11:47:43 +0200

    First commit with base website

---
+++ EfrohimTarnegolimWeb/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using EfrohimTarnegolimWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EfrohimTarnegolimWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
