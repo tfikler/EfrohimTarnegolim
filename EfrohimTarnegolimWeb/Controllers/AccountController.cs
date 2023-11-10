649d1df8c5bdbf01c7011b7aab847bd41ee092c9
Tomer Fikler <88192266+tfikler@users.noreply.github.com>
2023-11-10 16:34:58 +0200

    continue more, issue with the db write query

--- EfrohimTarnegolimWeb/Controllers/AccountController.cs
+++ EfrohimTarnegolimWeb/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using EfrohimTarnegolimWeb.Models;
+using EfrohimTarnegolimWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,27 @@ namespace EfrohimTarnegolimWeb.Controllers
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly RegisterDAO _registerDAO;
 
         public AccountController(ILogger<AccountController> logger)
         {
             _logger = logger;
+            _registerDAO = new RegisterDAO();
         }
 
+        
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SuccessLogin(UserModel userModel)
+        {
+            _registerDAO.InsertRegisterUserToDatabase(userModel);
+
+            // Redirect to a success page or handle as needed
+            return RedirectToAction("RegistrationSuccess");
         }
 
         public IActionResult SignIn()
---
+++ EfrohimTarnegolimWeb/Services/RegisterDAO.cs
@@ -0,0 +1,39 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using EfrohimTarnegolimWeb.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EfrohimTarnegolimWeb.Services
+{
+    public class RegisterDAO
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = Users;" +
+            "Integrated Security = True; Connect Timeout = 30; Encrypt=False;" +
+            "TrustServerCertificate=False;ApplicationIntent = ReadWrite; MultiSubnetFailover=False";
+        
+        public void InsertRegisterUserToDatabase(UserModel userModel)
+        {
+            string sqlStatement = "INSERT INTO Users (FullName, Email, Password) VALUES (@FullName, @Email, @Password)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.Add("@FullName", SqlDbType.VarChar, 40).Value = "tomer";
+                command.Parameters.AddWithValue("@Email", userModel.Email);
+                command.Parameters.AddWithValue("@Password", userModel.Password);
+                
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+            }
+        }
+    }
+}
